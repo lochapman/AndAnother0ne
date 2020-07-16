8b1ad4c0c692af3b13658871ac1e45c86eb05c34
Lukas Chapman <lukas.chapman@computingforall.org>
2020-07-16 15:18:03 -0700

    new stuff

---
+++ regex.js
@@ -0,0 +1,7 @@
+
+let changeIt =document.getElementById('thingThree').addEventListener('click',clickFunction())
+
+
+function clickFunction(){
+document.getElementById('thingTwo').classList.toggle('comicSans', 'levelsOfExodus')
+}
